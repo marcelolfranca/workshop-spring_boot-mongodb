77251d2fa69fbf9bfff9bbbafd05c83aaf9a1fae
marcelolfranca <marcelolfranca@gmail.com>
2018-10-24 15:51:28 -0300

    Deletar o usuario por ID com delete

--- src/main/java/com/marcelolfranca/workshopmongo/services/UserService.java
+++ src/main/java/com/marcelolfranca/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete (String id) {
+		findById(id); //aproveita a função já existente. Caso não exista o ID, já é lançada a excessão
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
